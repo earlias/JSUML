aa58412157e63399344eb4edc50c57003b72904f
Elias Earls <eearls@gmail.com>
2018-10-09 22:02:37 -0400

    update

--- Project/Palindrome/palindrome.js
+++ Project/Palindrome/palindrome.js
@@ -3,6 +3,11 @@ function reverseString(){
     var rString = "";
     for (var i = str.length - 1; i >= 0; i--) {
         rString += str[i];
+
+    if(str == rString) {
+        document.getElementById('target').innerHTML = 'That is a palindrome!';
+    } else {
+        document.getElementById('target').innerHTML = 'That is not a palindrome!';
+        }
     }
-    document.getElementById('target').innerHTML = rString;
 }
